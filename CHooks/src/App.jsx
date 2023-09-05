cdd5038aed6a8f5ab529c85ff18d627332d48548
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-05 11:05:53 +0200

    messy

--- CHooks/src/App.jsx
+++ CHooks/src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
+// import { useState, useEffect } from 'react'
+// import { CustomHooks } from './components/costumHooks'
 
-import './App.css'
+// import './App.css'
 
-function App() {
+// function App() {
 
-  return (
-    <>
-    <h1>Hello World</h1>
-    </>
-  )
-}
+//   return (
+//     <>
+//     <CustomHooks/>
+//     </>
+//   )
+// }
 
-export default App
+// export default App
+
+import { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+const Home = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      // .then((res) => res.json())
+      // .then((data) => setData(data));
+       .then(response => response.json())
+       .then(json => console.log(json))
+ }, []);
+
+//  fetch('https://jsonplaceholder.typicode.com/todos/1')
+//  .then(response => response.json())
+//  .then(json => console.log(json))
+
+return (
+  <>
+    {data &&
+      data.map((item) => {
+        return <p key={item.id}>{item.title}</p>;
+      })}
+  </>
+);
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Home />);
+
+export default Home
---
+++ CHooks/src/components/costumHooks.jsx
@@ -0,0 +1,20 @@
+// import { useState, useEffect } from 'react'
+
+// export const CustomHooks = (url) => {
+
+
+//     const [data] = useFetch("https://jsonplaceholder.typicode.com/todos/1");
+
+//     return (
+//         <>
+//           {data &&
+//             data.map((item) => {
+//               return <p key={item.id}>{item.title}</p>;
+//             })}
+//         </>
+//       );
+//     };
+
+// const root = CustomHooks.createRoot(document.getElementById('root'));
+// root.render(<CustomHooks />);
+
