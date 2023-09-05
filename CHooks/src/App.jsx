b86a85ac514590b1765d8837b8a5fd66fe94b761
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-09-05 12:20:52 +0200

    update

--- CHooks/src/App.jsx
+++ CHooks/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useFetch } from './components/customHooks'
+// import {UseFetch} from './components/codealong/useFetch'
 
 function App() {
   const [data] = useFetch("https://jsonplaceholder.typicode.com/todos")
 
 
+
 return (
   <>
+
   <h1>Hello world</h1>
   <h2>Lets build something together with custom hooks</h2>
   
